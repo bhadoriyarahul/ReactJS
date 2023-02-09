64c0c7221fdaa1f321128e66bbd4ef7997b9566e
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:46:03 +0530

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,14 @@
+import './Register.css';
+
+function Register() {
+
+  return (
+    <div id="content" >
+        <center>
+        <h1>Register Section</h1>
+        </center>
+    </div>
+  );
+}
+
+export default Register;
