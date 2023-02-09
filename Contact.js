327a6645bf93e437184211e593268d05b6e36df3
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:42:13 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,14 @@
+import './Contact.css';
+
+function Contact() {
+
+  return (
+    <div id="content" >
+        <center>
+        <h1>Contact Section</h1>
+        </center>
+    </div>
+  );
+}
+
+export default Contact;
