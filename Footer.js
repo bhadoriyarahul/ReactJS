05239d829ed8e8ae9110805041171952639136ef
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:43:13 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div id="footer" >
+    <center>
+    <h1>Footer Section</h1>
+    </center>
+    </div>
+  );
+}
+
+export default Footer;
