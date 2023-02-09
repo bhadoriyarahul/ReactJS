5cb58c32021f6de836e76e3f0961850caa921d4f
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:44:15 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,14 @@
+import './Login.css';
+
+function Login() {
+
+  return (
+    <div id="content" >
+        <center>
+        <h1>Login Section</h1>
+        </center>
+    </div>
+  );
+}
+
+export default Login;
