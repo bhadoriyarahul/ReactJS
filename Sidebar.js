c3ec2a1fc2008bcd2f73d73564d408292bd01019
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:47:07 +0530

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,13 @@
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div id="sidebar" >
+        <center>
+        <h1>Sidebar Section</h1>
+        </center>
+    </div>
+  );
+}
+
+export default Sidebar;
