a8a954b51bca9cd1369acd96011ead485c8f11e2
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-02-09 19:43:44 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div id="header" >
+    <center>
+    <h1>Header Section</h1>
+    <img src="assets/images/logo.png"  height="100" width="150" />
+    </center>
+    </div>
+  );
+}
+
+export default Header;
